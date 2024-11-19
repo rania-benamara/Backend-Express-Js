d5891048426a5ffbc82f36e043eeac4e341644cf
Rania Benamara <raniabenamara1101@gmail.com>
2024-11-19 14:20:09 -0500

    sprint6-v1

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const userRoutes = require("./routes/Clients/index");
 const offreRoutes = require("./routes/Offres/index");
 const produitRoutes = require("./routes/Product/index");
 
+
+const cors = require('cors');
+
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST','DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Pour les images spécifiquement
+app.use('/wp-content/uploads', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 // Middleware pour analyser les données JSON dans les requêtes
 app.use(express.json());
 
